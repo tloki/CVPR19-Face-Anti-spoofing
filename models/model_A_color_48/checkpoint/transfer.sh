2c726990cce69911f1e9db2096eb2f5fd6f18ba0
tloki <tomislav.lokotar@icloud.com>
2019-08-12 08:59:10 +0200

    add pretrained models

--- models/model_A_color_48/checkpoint/transfer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm global_min_acer_model.pth
-scp loki@ljiljana:/home/loki/Projects/rn/CVPR19-Face-Anti-spoofing/models/model_A_color_48/checkpoint/global_min_acer_model.pth \
-~/Projects/poso/02-CVPR19-Face-Anti-spoofing/models/model_A_color_48/checkpoint/
